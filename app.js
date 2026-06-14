1db5e6c389099bb9f046a2ed89917a9efc20fe85
soumikaaddala <addalasoumika@gmail.com>
2026-06-14 18:24:49 +0530

    added the problem panel

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import 'bpmn-js/dist/assets/diagram-js.css';
 import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css';
 import '@bpmn-io/properties-panel/dist/assets/properties-panel.css';
+import lintModule from 'bpmn-js-bpmnlint';
+import 'bpmn-js-bpmnlint/dist/assets/css/bpmn-js-bpmnlint.css';
 
+import bpmnlintConfig from './bundled-config.js';
 import CamundaBpmnModeler from 'camunda-bpmn-js/lib/camunda-platform/Modeler';
 
 import {
@@ -16,10 +19,66 @@ const modeler = new CamundaBpmnModeler({
   },
   additionalModules: [
     BpmnPropertiesPanelModule,
-    BpmnPropertiesProviderModule
-  ]
+    BpmnPropertiesProviderModule,
+    lintModule
+  ],
+   linting: {
+    bpmnlint: bpmnlintConfig
+  }
 });
+console.log(modeler.get('linting'));
+const eventBus = modeler.get('eventBus');
+eventBus.on('linting.completed', function(event) {
+  console.log('LINT EVENT FIRED');
+  console.log(event);
+  console.log(JSON.stringify(event.issues, null, 2));
+});
+eventBus.on('linting.completed', function (event) {
 
+  const tbody =
+    document.getElementById('problems-list');
+
+  tbody.innerHTML = '';
+
+  const issues = event.issues || [];
+
+  Object.entries(issues).forEach(([id, elementIssues]) => {
+
+    elementIssues.forEach(issue => {
+
+      const row =
+        document.createElement('tr');
+
+      row.innerHTML = `
+        <td>${issue.category}</td>
+        <td>${id}</td>
+        <td>${issue.message}</td>
+      `;
+
+      row.onclick = () => {
+
+        const elementRegistry =
+          modeler.get('elementRegistry');
+
+        const selection =
+          modeler.get('selection');
+
+        const canvas =
+          modeler.get('canvas');
+
+        const element =
+          elementRegistry.get(id);
+
+        if (element) {
+          selection.select(element);
+          canvas.scrollToElement(element);
+        }
+      };
+
+      tbody.appendChild(row);
+    });
+  });
+});
 const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <bpmn:definitions
 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
@@ -55,11 +114,42 @@ height="36"/>
 </bpmndi:BPMNDiagram>
 
 </bpmn:definitions>`;
+const problemsTab =
+  document.getElementById('problems-tab');
 
+const outputTab =
+  document.getElementById('output-tab');
+
+const problemsPanel =
+  document.getElementById('problems-panel');
+
+const outputPanel =
+  document.getElementById('output-panel');
+
+problemsTab.onclick = () => {
+  problemsPanel.style.display = 'block';
+  outputPanel.style.display = 'none';
+};
+
+outputTab.onclick = () => {
+  problemsPanel.style.display = 'none';
+  outputPanel.style.display = 'block';
+};
+
+function log(message) {
+  const output =
+    document.getElementById('output-text');
+
+  output.textContent +=
+    message + '\n';
+}
 async function openDiagram() {
   try {
     await modeler.importXML(xml);
     modeler.get('canvas').zoom('fit-viewport');
+ 
+
+console.log('Linting Activated');
   } catch (err) {
     console.error(err);
   }
